5c9f7c4af71889b200eab101fc89e5cc41eaef57
vpd.vlad <vpd.vlad@gmail.com>
2021-01-15 01:43:42 +0500

    strings full russian

--- src/strings/ru.js
+++ src/strings/ru.js
@@ -24,6 +24,77 @@ const ru = {
   // What we do
   "what-we-do-title": "Что мы делаем?",
   "what-we-do-info": "Мы помогаем матерям-одиночкам, детям-сиротам и иным уязвимым группам населения по всему Кыргызстану.  Семь приоритетных направлений нашей деятельности.",
+  "what-we-do-education": "Образование",
+  "what-we-do-education-info":"Доступ к достойному образованию",
+  "what-we-do-health-care": "Здравоохранение",
+  "what-we-do-health-care-info":"Забота и защита здоровья",
+  "what-we-do-culture": "Культура",
+  "what-we-do-culture-info":"Поддержка национальной культуры и наследия",
+  "what-we-do-eco":"Экология",
+  "what-we-do-eco-info":"Сохранение и поддержание экосистем",
+  "what-we-do-science":"Наука",
+  "what-we-do-science-info":"Развитие научно-иследовательских практик",
+  "what-we-do-economy":"Экономическое благосостояние",
+  "what-we-do-economy-info":"Расширение экономических возможностей",
+  "what-we-do-rights":"Правовая защита",
+  "what-we-do-rights-info":"Защита основных прав и свобод человека",
+
+  // donations amount
+
+  "donations-sum":"Общий сбор пожертвований",
+  "donations-kids":"Пожертовано детям",
+  "donations-mother":"Пожертвовано матерям-одиночками",
+  "donations-other":"Другие пожертвования",
+
+  //  Gallery
+  "gallery":"Галерея",
+
+
+  // donate now
+  "donate-now":"Сделать пожертвования сейчас",
+  "donate-now-info":"Присоединяйтесь к нам. Будьте опорой для матери и ребенка, уязвимым группам населения в Кыргызстане.",
+  "donate-button":"Пожертвовать",
+
+  // our projects
+  "project-1":"Проект 1",
+  "project-1-info":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
+  "project-2":"Проект 2",
+  "project-2-info":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
+
+  // send request 
+  "send-request":"Отправить запрос на получение помощи",
+  "send-request-info":"Общественный фонд «Умут булагы» помогает матерям-одиночкам, детям-сиротам и иным уязвимым группам населения по всей стране в виде материальных и нематериальных благ.",
+  "send-request-button":"Отправить",
+
+  // join ymyt
+  "join":"Присоединиться к Умγт булагы",
+  "join-info":"Женщины и дети, другие социально уязвимые группы Кыргызстана особенно нуждаются в поддержке. Возьмите инициативу, помогите нашему сообществу, став волонтером или партнером Умут булагы.",
+  "join-button":"Присоединиться",
+
+  // operating mode 
+  "work":"Режим работы",
+  "work-days":"Пн-Пт:",
+  "work-time":"09:00 - 18:00",
+  "work-break":"Перерыв:",
+  "work-break-time":"13:00 - 14:00",
+  "work-saturday":"Суббота:",
+  "work-saturday-time":"09:00 - 16:00",
+  "work-sunday":"Воскресенье:",
+  "work-sunday-time":"Выходной",
+
+  // footer
+  "footer-links":"Быстрые ссылки",
+  "footer-news":"Новости",
+  "footer-projects":"Проекты",
+  "footer-about-us":"О нас",
+  "footer-adress":"Адрес",
+  "footer-city":"720031, Кыргызстан, г. Бишкек",
+  "footer-street":"ул. Ибраимова 100, 6 этаж",
+  "footer-contacts":"Контактные данные",
+  "footer-phone":"+996(501)345-777",
+  "footer-email":"ubulagy@gmail.com ",
+
+
 };
 
 export default ru

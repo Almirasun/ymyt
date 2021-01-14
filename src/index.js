d6187aeb07961d97dd87af75bf00c72f7e382918
Almira Sagynbek <almira.sagynbek@gmail.com>
2021-01-14 20:01:08 +0600

    chore: added social icons

--- src/index.js
+++ src/index.js
@@ -1,78 +1,72 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
 
-import Cover from "./components/cover/cover";
-import Navbar from "./components/navbar/navbar";
+import * as Components from './components';
 
-import "./index.css";
-
-class App extends Component {
-  render() {
-    return (
-      <main className="App">
-        <section className="first-section">
-          <header className="header">
-            <div className="logo">
-              Логотип
+function App() {
+  return (
+    <main className="App">
+      <section className="first-section">
+        <header className="header">          
+            <div className="logo">Логотип
               {/* <img src="logo.svg"></img> */}
             </div>
             <div>
               <nav>
-                <ul class="menu">
-                  <li class="menu-item">RU</li>
-                  <li class="menu-item">KG</li>
-                  <li class="menu-item">EN</li>
+                <ul className="menu">
+                  <li className="menu-item">RU</li>
+                  <li className="menu-item">KG</li>
+                  <li className="menu-item">EN</li>
                 </ul>
               </nav>
               <menu>Меню</menu>
             </div>
           </header>
           <div className="main-text">
-            <p className="main-text__headline">Үмүт булагы</p>
+            <p className="main-text__headline">Үмүт булагы</p> 
             <p className="main-text__caption">Эне-балага тирек</p>
           </div>
           <button className="contact-us">Связаться с нами</button>
-          <div>
+          <div className="social-icons__area">
             <p>Следите за нами:</p>
-            <ul class="icons">
-              <li class="icons-item">Facebook</li>
-              <li class="icons-item">Twitter</li>
-              <li class="icons-item">Instagram</li>
-            </ul>
-          </div>
-          {/* <img src={kids} alt=""/>   */}
-        </section>
+            <Components.SocialIcons /> 
+          </div>        
+      </section>
 
-        <section className="second-section">
-          <div>
-            <p className="second-section__title">Кто мы?</p>
-            <div className="">
-              <p className="second-section__description">
-                Общественный фонд "Умут булагы" под инициативой первой леди
-                Кыргызской Республики Айгуль Жапаровой работает по всей стране.
-                "Мы стремимся в первую очередь помогать детям и матерям.
-                Улучшать их жизни, защищать и развивать их потенциал. Умут
-                булагы – опора для матери и ребенка." – А. Жапарова.
-              </p>
-            </div>
-          </div>
-        </section>
-
-        <section>
-          <div>
-            <h3>Что мы делаем?</h3>
-            <p>
-              Мы помогаем матерям-одиночкам, детям-сиротам и иным уязвимым
-              группам населения по всему Кыргызстану. Семь приоритетных
-              направлений нашей деятельности.
+      <section className="second-section">
+        <div className="second-section__area">
+          <p className="second-section__title">Кто мы?</p>
+          <p className="second-section__description">
+              Общественный фонд "Умут булагы" под инициативой первой леди
+              Кыргызской Республики Айгуль Жапаровой работает по всей стране.
+              "Мы стремимся в первую очередь помогать детям и матерям. Улучшать 
+              их жизни, защищать и развивать их потенциал. Умут булагы – опора для 
+              матери и ребенка." –  А. Жапарова.
             </p>
-          </div>
-          <div className="activities"></div>
-        </section>
-        <footer></footer>
-      </main>
-    );
-  }
+        </div>
+      </section>
+
+      <section className="third-section">  
+        <div className="third-section__area">
+          <p className="third-section__title">Что мы делаем?</p>
+          <p className="third-section__description">
+            Мы помогаем матерям-одиночкам, детям-сиротам и иным уязвимым 
+            группам населения по всему Кыргызстану.  Семь приоритетных 
+            направлений нашей деятельности. 
+          </p>
+        </div>
+        <div className="activities">
+
+        </div>
+      </section>
+
+      <footer></footer>
+    </main>
+  )  
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render((
+    <App />
+),  document.getElementById('root'));
+

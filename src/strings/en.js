db88721e7784e0e1bdefc051dd4a2f7e70b765b8
vpd.vlad <vpd.vlad@gmail.com>
2021-01-24 19:17:32 +0500

    new Strings

--- src/strings/en.js
+++ src/strings/en.js
@@ -14,6 +14,102 @@ const en = {
   "cover-title": "Yмγт бyлагы",
   "cover-moto": "Эне-балага тирек",
   "cover-contact-us-button": "Contact us",
+  "cover-follow-us":"Follow us:",
+
+
+  // Who we are
+  "who-we-are-title": "Who we are?",
+  "who-we-are-info": 'A public foundation "Umut bulagy" works throughout the country under the initiative of First Lady of the Kyrgyz Republic Aigul Zhaparova. "We primarily strive to help children and mothers. To improve their quality of life, to protect and empower them. Umut bulagy – is a pillar for mother and child". A. Zhaparova',
+
+  // Mission
+  "mission-title":"Our mission",
+  "mission-info":"Supporting the creation of essential conditions for the health and comprehensive development of single mothers, orphans and other vulnerable groups in Kyrgyzstan to achieve social justice and impartiality in the society.",
+  
+
+  // What we do
+  "what-we-do-title": "What we do?",
+  "what-we-do-info": "We help single mothers, orphans and other vulnerable groups of the society across Kyrgyzstan. Seven priority areas of our work are:",
+  "what-we-do-education": "Education",
+  "what-we-do-education-info":"Access to a quality education",
+  "what-we-do-health-care": "Public health",
+  "what-we-do-health-care-info":"Healthcare",
+  "what-we-do-culture": "Culture",
+  "what-we-do-culture-info":"Cultural diversity and heritage support",
+  "what-we-do-eco":"Ecology",
+  "what-we-do-eco-info":"Protection and maintenance of ecosystems",
+  "what-we-do-science":"Science",
+  "what-we-do-science-info":"Development of research practices",
+  "what-we-do-economy":"Economic well-being",
+  "what-we-do-economy-info":"Economic empowerment",
+  "what-we-do-rights":"Legal protection",
+  "what-we-do-rights-info":"Fundamental human and civil rights and freedoms protection",
+
+  // donations amount
+  "donations-sum":"Общий сбор пожертвований",
+  "donations-kids":"Пожертовано детям",
+  "donations-mother":"Пожертвовано матерям-одиночками",
+  "donations-other":"Другие пожертвования",
+
+  //  Gallery
+  "gallery-title":"Gallery",
+
+
+  // donate now
+  "donate-now":"Donate Now",
+  "donate-now-info":"Join us. Support mothers and children, vulnerable groups in Kyrgyzstan.",
+  "donate-button":"Donate",
+  "donate-button-info" : "Next you will be taken to a secure payment page, where you can transfer your donation.",
+
+
+  // our projects
+  "project-1":"Project 1",
+  "project-1-info":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
+  "project-2":"Project 2",
+  "project-2-info":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
+
+  // send request 
+  "send-request":"Submit a request for assistance",
+  "send-request-info":'"Umut bulagy" Public Foundation helps single mothers, orphans and other vulnerable groups throughout the country with tangible and intangible benefits.',
+  "send-request-button":"Submit",
+  "send-request-response": "Thank you for your request. We will review and contact you as soon as possible.",
+
+  // join ymyt
+  "join":'Join "Umut bulagy"',
+  "join-info":'Women and children, other vulnerable social groups in Kyrgyzstan especially need your support. Take the initiative and help our community by becoming a volunteer or partner of Umut bulagy.',
+  "join-button":"Join",
+
+  // operating mode 
+  "work":"Режим работы",
+  "work-days":"Пн-Пт:",
+  "work-time":"09:00 - 18:00",
+  "work-break":"Перерыв:",
+  "work-break-time":"13:00 - 14:00",
+  "work-saturday":"Суббота:",
+  "work-saturday-time":"09:00 - 16:00",
+  "work-sunday":"Воскресенье:",
+  "work-sunday-time":"Выходной",
+
+  // footer
+  "footer-links":"Быстрые ссылки",
+  "footer-news":"Новости",
+  "footer-projects":"Проекты",
+  "footer-about-us":"О нас",
+  "footer-adress":"Адрес",
+  "footer-city":"720031, Кыргызстан, г. Бишкек",
+  "footer-street":"ул. Ибраимова 100, 6 этаж",
+  "footer-contacts":"Контактные данные",
+  "footer-phone":"+996(501)345-777",
+  "footer-email":"ubulagy@gmail.com ",
+
+
+  // history
+  "history":"История",
+  "history-info":"Общественный фонд \"Умут булагы\" был основан в 2020 году инициативой первой леди Кыргызской Республики Айгуль Жапаровой. Фонд получил официальное название – Умут булагы, в честь молодого парня по имени Умут, погибшего на мирном митинге в октябре 2020 г. в Кыргызстане. Наш фонд символизирует источник надежды и веру личности в себя, в страну и в лучшее будущее.",
+  
+  // appeal
+  "appeal-title":"Обращение первой леди Айгуль Жапаровой",
+  "appeal-info":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard text ever since the 1500s, when an unknown printer took a galley of type and. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and Lorem Ipsum is simply dummy text of the printing and typesetting industry.d 1500s, when an unknown printer took a galley of type and Lorem Ipsum is simply dummy text of the printing and typesetting industry.d psum is simply dummy text of the printing and typesetting industry.",
+  
 };
 
 export default en

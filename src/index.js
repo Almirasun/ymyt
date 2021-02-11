e149693d662fbc99704586a971f0ab9f770107f8
Aibek Mazhitov <amazhitov@gmail.com>
2021-02-11 21:52:50 +0600

    fix: fixed scroll to top action on route change

--- src/index.js
+++ src/index.js
@@ -3,26 +3,36 @@ import ReactDOM from "react-dom";
 import * as AppRedux from "./redux";
 import * as ReactRedux from "react-redux";
 import * as Components from "./components";
-import {BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter, Route, withRouter } from "react-router-dom";
 import "./index.scss";
 
-function App() {
+function App({ history }) {
+  React.useEffect(() => {
+    history.listen(() => {
+      window.scrollTo(0, 0);
+    });
+  }, [])
+
   return (
-    <BrowserRouter>
-        <Route exact path="/" component={Components.HomePage} />
-        <Route path="/about-us" component={Components.AboutUsPage} />
-        <Route path="/donateForm" component={Components.DonateGoogleForm} />
-        <Route path="/requestForm" component={Components.RequestGoogleForm} />
-        <Route path="/joinForm" component={Components.JoinGoogleForm} />
-        <Route path="/visitForm" component={Components.VisitGoogleForm} />
-        <Route path="/newsPage" component={Components.NewsPage} />
-    </BrowserRouter>
+    <>
+      <Route exact path="/" component={Components.HomePage} />
+      <Route path="/about-us" component={Components.AboutUsPage} />
+      <Route path="/donateForm" component={Components.DonateGoogleForm} />
+      <Route path="/requestForm" component={Components.RequestGoogleForm} />
+      <Route path="/joinForm" component={Components.JoinGoogleForm} />
+      <Route path="/visitForm" component={Components.VisitGoogleForm} />
+      <Route path="/newsPage" component={Components.NewsPage} />
+    </>
   );
 }
 
+const AppRouter = withRouter(App);
+
 ReactDOM.render(
   <ReactRedux.Provider store={AppRedux.store}>
-    <App />
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   </ReactRedux.Provider>,
   document.getElementById("root")
 );
